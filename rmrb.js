3b75cf2ac9b7d5f9ad7c176e670bb62f22d80501
WuHanMuMu <1027480600@qq.com>
2016-10-13 16:42:39 +0800

    Update rmrb.js

--- rmrb.js
+++ rmrb.js
@@ -1,14 +1,7 @@
 /**
  * Created by wang on 2016/10/10.
  */
-`http://paper.people.com.cn/rmrb/html/2016-10/10/nw.D110000renmrb_20161010_1-01.htm
-http://paper.people.com.cn/rmrb/html/2016-10/09/nw.D110000renmrb_20161009_1-02.htm
-http://paper.people.com.cn/rmrb/html/2016-10/10/nbs.D110000renmrb_01.htm
-http://paper.people.com.cn/rmrb/html/2016-10/09/nbs.D110000renmrb_01.htm
-http://www.cs.com.cn/
-http://www.cnstock.com/
-http://www.capitalweek.com.cn/
-http://www.stcn.com/`
+`冒得注释`
 var buffer=require('buffer').Buffer
 var iconv=require('iconv-lite')
 var http=require('http')
